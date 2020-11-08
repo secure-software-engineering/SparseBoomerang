55497d6e7a32d1cbcc1eec33bcd629f18283d198
Johannes Spaeth <johannes.spaeth@codeshield.de>
2020-11-09 00:14:29 +0100

    Test case for #5

--- boomerangPDS/src/test/java/test/cases/bugfixes/Repro.java
+++ boomerangPDS/src/test/java/test/cases/bugfixes/Repro.java
@@ -1,4 +1,4 @@
-package test;
+package test.cases.bugfixes;
 
 import boomerang.Boomerang;
 import boomerang.BoomerangOptions;
@@ -7,9 +7,12 @@ import boomerang.results.ForwardBoomerangResults;
 import boomerang.scene.*;
 import boomerang.scene.ControlFlowGraph.Edge;
 import boomerang.scene.jimple.*;
+import com.google.common.collect.Sets;
 import com.google.common.collect.Table;
 import com.google.common.collect.Table.Cell;
+import java.util.Map.Entry;
 import java.util.Set;
+import org.junit.Test;
 import soot.*;
 import soot.jimple.AssignStmt;
 import soot.options.Options;
@@ -20,10 +23,29 @@ import java.util.Map;
 import wpds.impl.Weight.NoWeight;
 import wpds.interfaces.State;
 
+/**
+ * This code was added to test https://github.com/CodeShield-Security/SPDS/issues/5.
+ * Thanks @copumpkin for sharing code for testing purpose.
+ */
 public class Repro {
   static BoomerangOptions opts = new IntAndStringBoomerangOptions();
 
-  private static void setupSoot(String classPath) {
+  @Test
+  public void excludeFoo() {
+    G.reset();
+    setupSoot("src/test/resources/Test.jar", true);
+    analyze("<Foo: void baz()>", "<Foo: void bar()>", "<Foo: void <init>()>");
+  }
+
+  @Test
+  public void includeFoo() {
+    G.reset();
+    setupSoot("src/test/resources/Test.jar", false);
+    analyze("<Foo: void baz()>", "<Foo: void bar()>", "<Foo: void <init>()>", "<java.lang.Object: void <init>()>");
+  }
+
+
+  private static void setupSoot(String classPath, boolean excludeFoo) {
     Options.v().set_whole_program(true);
     Options.v().setPhaseOption("cg.spark", "on");
     Options.v().set_no_bodies_for_excluded(true);
@@ -31,7 +53,9 @@ public class Repro {
     Options.v().set_keep_line_number(true);
 
     /* ********* Uncomment this line to see methods invoked on Foo! ********* */
-    // Options.v().set_exclude(Collections.singletonList("Foo"));
+    if(excludeFoo) {
+      Options.v().set_exclude(Collections.singletonList("Foo"));
+    }
 
     Options.v().setPhaseOption("jb", "use-original-names:true");
     Options.v().set_soot_classpath(classPath);
@@ -40,39 +64,28 @@ public class Repro {
     Scene.v().loadNecessaryClasses();
   }
 
-  private static void analyze() {
-    PackManager.v().getPack("wjtp").add(new Transform("wjtp.repro", new ReproTransformer()));
+  private static void analyze(String... expectedCallSignatureOnFoo) {
+    PackManager.v().getPack("wjtp").add(new Transform("wjtp.repro", new ReproTransformer(expectedCallSignatureOnFoo)));
     PackManager.v().getPack("cg").apply();
     PackManager.v().getPack("wjtp").apply();
   }
 
-  public static void main(String[] args) {
-    setupSoot("Test.jar");
-    analyze();
-  }
 
   private static Map<Edge, DeclaredMethod> getMethodsInvokedFromInstanceInStatement(Statement queryStatement) {
     Val var = new AllocVal(queryStatement.getLeftOp(), queryStatement, queryStatement.getRightOp());
     ForwardQuery fwq = new ForwardQuery(new Edge(queryStatement, queryStatement.getMethod().getControlFlowGraph().getSuccsOf(queryStatement).stream().findFirst().get()),var);
     Boomerang solver = new Boomerang(new SootCallGraph(), SootDataFlowScope.make(Scene.v()), opts);
     ForwardBoomerangResults<NoWeight> results = solver.solve(fwq);
-    Table<ControlFlowGraph.Edge, Val, NoWeight> statementValNoWeightTable = results.asStatementValWeightTable();
-    for(Cell<ControlFlowGraph.Edge, Val, NoWeight> c :
-    statementValNoWeightTable.cellSet()){
-      //  System.out.println(c.getRowKey() +" " + c.getColumnKey());
-      ControlFlowGraph.Edge edge = c.getRowKey();
-      Statement s = edge.getStart();
-      if(s.containsInvokeExpr()){
-        if(s.getInvokeExpr().isInstanceInvokeExpr() && s.getInvokeExpr().getBase().equals(c.getColumnKey())){
-           System.out.println(s + "  " + c.getColumnKey());
-        }
-      }
-    }
-
     return results.getInvokedMethodOnInstance();
   }
 
   static class ReproTransformer extends SceneTransformer {
+
+    Set<String> expectedCalledMethodsOnFoo;
+    public ReproTransformer(String... expectedCallSignatureOnFoo) {
+      expectedCalledMethodsOnFoo = Sets.newHashSet(expectedCallSignatureOnFoo);
+    }
+
     @Override
     protected void internalTransform(String name, Map<String, String> options) {
       BoomerangPretransformer.v().reset();
@@ -88,22 +101,19 @@ public class Repro {
 
       Statement newFoo = method.getControlFlowGraph().getStatements().stream().filter(
           x -> x.toString().contains("$stack2 = new Foo")).findFirst().get();
-      Statement newList = method.getControlFlowGraph().getStatements().stream().filter(
-          x -> x.toString().contains("$stack4 = new LinkedList")).findFirst().get();
 
       // This will only show results if set_exclude above gets uncommented
       System.out.println("\nFoo invoked methods:");
-      for (Map.Entry<Edge, DeclaredMethod> e : getMethodsInvokedFromInstanceInStatement(newFoo).entrySet()) {
+      Set<Entry<Edge, DeclaredMethod>> entries = getMethodsInvokedFromInstanceInStatement(newFoo)
+          .entrySet();
+      Set<String> methodCalledOnFoo = Sets.newHashSet();
+      for (Map.Entry<Edge, DeclaredMethod> e : entries) {
         System.out.println("\t" + e.getKey().toString());
         System.out.println("\t\t" + e.getValue().toString());
+        methodCalledOnFoo.add(e.getValue().toString());
       }
 
-      // This will show results regardless of the exclusion
-      System.out.println("\nList invoked methods:");
-      for (Map.Entry<Edge, DeclaredMethod> e : getMethodsInvokedFromInstanceInStatement(newList).entrySet()) {
-        System.out.println("\t" + e.getKey().toString());
-        System.out.println("\t\t" + e.getValue().toString());
-      }
+      assert methodCalledOnFoo.equals(Sets.newHashSet(expectedCalledMethodsOnFoo));
     }
   }
 }
