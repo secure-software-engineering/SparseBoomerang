9716c36d0f5d661a8823042792f63da6fee80834
Kadiray Karakaya <kadiray.k@hotmail.com>
2022-06-15 16:25:23 +0200

    querylog for AliasAwareSparseCFGCache

--- boomerangScope/src/main/java/boomerang/scene/sparse/aliasaware/AliasAwareSparseCFGCache.java
+++ boomerangScope/src/main/java/boomerang/scene/sparse/aliasaware/AliasAwareSparseCFGCache.java
@@ -7,6 +7,8 @@ import boomerang.scene.sparse.SootAdapter;
 import boomerang.scene.sparse.SparseAliasingCFG;
 import boomerang.scene.sparse.SparseCFGCache;
 import boomerang.scene.sparse.eval.SparseCFGQueryLog;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,6 +17,8 @@ import soot.Value;
 import soot.jimple.Stmt;
 
 public class AliasAwareSparseCFGCache implements SparseCFGCache {
+
+  List<SparseCFGQueryLog> logList = new ArrayList<>();
 
   Map<String, SparseAliasingCFG> cache;
   AliasAwareSparseCFGBuilder sparseCFGBuilder;
@@ -40,6 +44,9 @@ public class AliasAwareSparseCFGCache implements SparseCFGCache {
       if (s.startsWith(m.getSignature())) {
         SparseAliasingCFG sparseAliasingCFG = cache.get(s);
         if (sparseAliasingCFG.getGraph().nodes().contains(stmt)) {
+          SparseCFGQueryLog queryLog =
+                  new SparseCFGQueryLog(true, SparseCFGQueryLog.QueryDirection.FWD);
+          logList.add(queryLog);
           return sparseAliasingCFG;
         }
       }
@@ -71,21 +78,37 @@ public class AliasAwareSparseCFGCache implements SparseCFGCache {
 
     if (cache.containsKey(key)) {
       if (cache.get(key).getGraph().nodes().contains(sootCurrentStmt)) {
+        SparseCFGQueryLog queryLog =
+                new SparseCFGQueryLog(true, SparseCFGQueryLog.QueryDirection.BWD);
+        logList.add(queryLog);
         return cache.get(key);
       } else {
+        SparseCFGQueryLog queryLog =
+                new SparseCFGQueryLog(false, SparseCFGQueryLog.QueryDirection.BWD);
+        queryLog.logStart();
         SparseAliasingCFG cfg =
             sparseCFGBuilder.buildSparseCFG(
                 sootInitialQueryVal, sootSurrentMethod, sootCurrentQueryVal, sootCurrentStmt);
+        queryLog.logEnd();
         cache.put(key + currentStmt, cfg);
+        logList.add(queryLog);
         return cfg;
       }
     } else if (cache.containsKey(key + currentStmt)) {
+      SparseCFGQueryLog queryLog =
+              new SparseCFGQueryLog(true, SparseCFGQueryLog.QueryDirection.BWD);
+      logList.add(queryLog);
       return cache.get(key + currentStmt);
     } else {
+      SparseCFGQueryLog queryLog =
+              new SparseCFGQueryLog(false, SparseCFGQueryLog.QueryDirection.BWD);
+      queryLog.logStart();
       SparseAliasingCFG cfg =
           sparseCFGBuilder.buildSparseCFG(
               sootInitialQueryVal, sootSurrentMethod, sootCurrentQueryVal, sootCurrentStmt);
+      queryLog.logEnd();
       cache.put(key, cfg);
+      logList.add(queryLog);
       return cfg;
     }
   }
