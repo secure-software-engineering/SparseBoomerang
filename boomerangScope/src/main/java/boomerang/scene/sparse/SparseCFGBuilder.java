adf27bee27908837033547888f5cb67622363d50
Kadiray Karakaya <kadiray.k@hotmail.com>
2024-08-07 12:06:27 +0200

    fix new guava invalidating preds and succs

--- boomerangScope/src/main/java/boomerang/scene/sparse/SparseCFGBuilder.java
+++ boomerangScope/src/main/java/boomerang/scene/sparse/SparseCFGBuilder.java
@@ -91,4 +91,18 @@ public class SparseCFGBuilder {
     }
     return false;
   }
+
+  protected void removeStmt(MutableGraph<Unit> mCFG, Unit unit) {
+    Set<Unit> preds = mCFG.predecessors(unit);
+    List<Unit> tmpPreds = new ArrayList<>();
+    preds.forEach(e->tmpPreds.add(e));
+    Set<Unit> succs = mCFG.successors(unit);
+    List<Unit> tmpSuccs = new ArrayList<>();
+    succs.forEach(e->tmpSuccs.add(e));
+    if (tmpPreds.size() == 1 && tmpSuccs.size() == 1) {
+      mCFG.removeNode(unit);
+      mCFG.putEdge(tmpPreds.get(0), tmpSuccs.get(0));
+    }
+  }
+
 }
