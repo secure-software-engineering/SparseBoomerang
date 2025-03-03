7e97a42598da0e7db2e58212ee16bf0b9f84d9af
akshaya <akshayapuradam@gmail.com>
2025-03-03 22:22:18 +0100

    added TransitionRepresentationFunction.java

---
+++ idealPDS/src/main/java/typestate/TransitionRepresentationFunction.java
@@ -0,0 +1,101 @@
+/**
+ * ***************************************************************************** Copyright (c) 2018
+ * Fraunhofer IEM, Paderborn, Germany. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * <p>SPDX-License-Identifier: EPL-2.0
+ *
+ * <p>Contributors: Johannes Spaeth - initial API and implementation
+ * *****************************************************************************
+ */
+package typestate;
+
+import boomerang.scope.ControlFlowGraph.Edge;
+import com.google.common.collect.Sets;
+import typestate.finiteautomata.ITransition;
+import wpds.impl.Weight;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TransitionRepresentationFunction extends Weight {
+
+
+
+  private final String rep;
+
+  private static TransitionRepresentationFunction one;
+
+  private static TransitionRepresentationFunction zero;
+
+  private final Set<Edge> stateChangeStatements;
+
+  public TransitionRepresentationFunction(Set<Edge> stateChangeStatements) {
+    this.stateChangeStatements = stateChangeStatements;
+    this.rep = null;
+  }
+
+
+  private TransitionRepresentationFunction(String rep) {
+    this.rep = rep;
+    this.stateChangeStatements = Sets.newHashSet();
+  }
+
+  public Set<Edge> getLastStateChangeStatements() {
+    return stateChangeStatements;
+  }
+
+  @Override
+  public Weight extendWith(Weight other) {
+    if (other.equals(one())) return this;
+    if (this.equals(one())) return other;
+    if (other.equals(zero()) || this.equals(zero())) {
+      return zero();
+    }
+    throw new IllegalStateException("This should not happen!");
+     }
+
+  @Override
+  public Weight combineWith(Weight other) {
+    if (!(other instanceof TransitionRepresentationFunction)) throw new RuntimeException();
+    if (this.equals(zero())) return other;
+    if (other.equals(zero())) return this;
+    if (other.equals(one()) && this.equals(one())) {
+      return one();
+    }
+    TransitionRepresentationFunction func = (TransitionRepresentationFunction) other;
+    if (other.equals(one()) || this.equals(one())) {
+     Set<ITransition> idTransitions = Sets.newHashSet();
+    }
+
+    HashSet<Edge> newStateChangeStmts = Sets.newHashSet(stateChangeStatements);
+    newStateChangeStmts.addAll(func.stateChangeStatements);
+    Weight weight = extendWith(other);
+    return weight;
+  }
+
+  public static TransitionRepresentationFunction one() {
+    if (one == null) one = new TransitionRepresentationFunction("ONE");
+    return one;
+  }
+
+  public static TransitionRepresentationFunction zero() {
+    if (zero == null) zero = new TransitionRepresentationFunction("ZERO");
+    return zero;
+  }
+
+  public String toString() {
+    return (this==one)? "ONE" : "ZERO";
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((rep == null) ? 0 : rep.hashCode());
+    return result;
+  }
+
+
+}
